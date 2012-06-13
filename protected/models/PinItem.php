f0cead6b61a4cc490991dfbc985db645364f189f
monthas <monsiree.t@gmail.com>
2012-06-13 16:51:29 +0700

    add rule for pin item model add actionAdd in pin item controller add view for pin item add page add view for category add page

--- protected/models/PinItem.php
+++ protected/models/PinItem.php
@@ -15,9 +15,26 @@
 				'board'=>array(self::BELONGS_TO, 'Board', 'board_id'),
 				'comments'=>array(self::HAS_MANY, 'Comment', 'pin_item_id'),
 				'pin_item_liked_users'=>array(self::HAS_MANY, 'PinItemLikedUser', 'pin_item_id'),
-				'liked_users'=>array(self::HAS_MANY, 'User', array('user_id'=>'id'), 'throght'=> 'pin_item_liked_users'),
+				'liked_users'=>array(self::HAS_MANY, 'User', array('user_id'=>'id'), 'through'=> 'pin_item_liked_users'),
 				
 			);
 		}
+		
+		public function rules() {
+			return array(
+				array('img_src, user_id', 'required'),
+				array('user_id, width, height', 'numerical'),
+				array('img_src', 'url')
+			);
+		}
+		
+		public function beforeSave() {
+			if ($this->isNewRecord) {
+				list($this->width, $this->height) = getimagesize($this->img_src);
+			}
+			
+			return true;
+		} 
+		
 	}
 ?>
