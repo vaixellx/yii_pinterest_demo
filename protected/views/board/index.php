5381029ec73e4fde9f1e62428e303682aa76acb8
thethak <thethak@gmail.com>
2012-06-29 12:20:59 +0700

    - add action index and edit to BoardController - add action board to PinItemController (show pins in each board) - add getCoverImage() in Board model - add beforeDelete() to Board model and PinItem model - add edit and index to board view.

--- protected/views/board/index.php
+++ protected/views/board/index.php
@@ -1,10 +1,17 @@
-<?php
-$this->breadcrumbs=array(
-	'Board',
-);?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-
-<p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
-</p>
+<?php $this->widget('application.components.widgets.MainMenuWidget'); ?>
+<div class="center" style="margin: 15px;">
+	<?php foreach($boards as $board): ?>
+		<div class="user-board">
+			<div class="user-board-header"><?php echo $board->title; ?></div>
+			<div class="user-board-count">
+				<span style="float: left"><?php echo CHtml::link('Edit', array('edit', 'id'=>$board->id)); ?></span>
+				<span style="float: right"><?php echo count($board->pinItems); ?> Items</span>
+			</div>
+			<?php echo CHtml::link('', array('/pinitem/board', 'id'=>$board->id), array('class'=>'board-link')); ?>
+			<div class="user-board-cover"><?php $pin_img = $board->getCoverImage(); echo CHtml::Image(@$pin_img[0]); ?></div>
+			<div class="user-board-small">
+				<?php for($i=1; $i<sizeof($pin_img); $i++): ?><span class="user-board-smallpic"><?php echo CHtml::Image($pin_img[$i]); ?></span><?php endfor; ?>
+			</div>
+		</div>
+	<?php endforeach; ?>
+</div>
